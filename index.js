b6f35d58072fc3885dd64cd414a9365951bd5d72
Elton Gonçalves <eltongoncalvesdeoliveira@hotmail.com>
2025-06-02 22:16:43 -0300

    test: rota de teste-api no index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const authRoutes = require('./routes/auth');
 const vendasRoutes = require('./routes/vendas');
+const organizacaoRoutes = require('./routes/organizacao'); // <-- Adicionada esta linha
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,11 @@ app.use(bodyParser.json());
 // Rotas
 app.use('/auth', authRoutes);
 app.use('/vendas', vendasRoutes);
+app.use('/api/organizacao', organizacaoRoutes); // <-- Adicionada esta linha
+
+app.get('/teste-api', (req, res) => {
+  res.send('Teste direto do index OK');
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
